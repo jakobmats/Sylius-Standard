593939e535f7bb3b57f24e5f27b6f0a752158c1e
Jakub Groncki <j.groncki@getprintbox.com>
2021-09-17 17:21:28 +0200

    Initial commit

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -14,8 +14,36 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  */
 class Product extends BaseProduct
 {
+    public const COLOR_RED = 'Red';
+
+    public const COLOR_GREEN = 'Green';
+
+    public const COLOR_BLUE = 'Blue';
+
+    /** @ORM\Column(type="string", columnDefinition="ENUM('red', 'green', 'blue')", nullable=true) */
+    protected ?string $color;
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public static function getAvailableColors(): array
+    {
+        return [
+            self::COLOR_RED => 0,
+            self::COLOR_GREEN => 1,
+            self::COLOR_BLUE => 2,
+        ];
     }
 }
---
+++ src/Form/Extension/ProductTypeExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form\Extension;
+
+use App\Entity\Product\Product;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ProductTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('color', ChoiceType::class, [
+                'choices' => Product::getAvailableColors(),
+            ]);
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ProductType::class];
+    }
+}
